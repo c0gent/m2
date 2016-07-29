3f7ebcccef2a23754fc0244b68408aad72b6b1b4
Nick Sanders <cogciprocate@gmail.com>
2016-07-29 10:17:25 -0700

    Include other, incomplete modules.

---
+++ space_map/src/lib.rs
@@ -0,0 +1,19 @@
+//! A map of 3d space which acts as an intermediate between a classical serial
+//! model of storage and a {{2d parallel model}} which can be mapped onto other
+//! parallelized maps.
+//!
+//!
+
+pub struct SpaceMap {
+
+}
+
+
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_nothing() {
+        println!("Absolutely nothing completed.");
+    }
+}
